32c6220833420c042b1c6a1002e37b2f5c3a66d9
pushpahiremath77 <pushpahiremath20@gmail.com>
2024-12-20 10:30:46 +0530

    UC1 for creating a contact

--- address_book.py
+++ address_book.py
@@ -1,6 +1,35 @@
+class Contact:
+    def __init__(self, first_name="", last_name="", address="", city="", state="", zip="", phone_num="", email=""):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.phone = phone_num
+        self.email = email
+
+    def create_contact(self):
+        self.first_name = input("Enter First Name: ")
+        self.last_name = input("Enter Last name: ")
+        self.address = input("Enter your Address: ")
+        self.city = input("Enter your city: ")
+        self.state = input("Enter your state: ")
+        self.zip = input("Enter your pin code: ")
+        self.phone = input("Enter phone number: ")
+        self.email = input("Enter your email address: ")
+        
+    def __str__(self):
+        return (f"Contact created for {self.first_name} {self.last_name}")
+
+contact1 = Contact()
+contact1.create_contact()
+print(contact1)
+
 class AddressBookMain:
 
-    def __init__(self):
-        print("Welcome to Address Book Program")
+    def display(self):
+        print("Welcome to Address Book program in AddressBookMain class")
 
-addressbook = AddressBookMain()
+address_book_obj = AddressBookMain()
+address_book_obj.display()
